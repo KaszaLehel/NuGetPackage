f3fe484566f3e0a178f338ab9bed2fd4b5ffda9b
Berserk012 <gamerleaguelol4@gmail.com>
2023-03-07 23:52:56 +0100

    first commit

---
+++ Library/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Library
+{
+    public class Class1
+    {
+         public static int Add(int i, int j)
+        {
+            return i+j;
+        }
+        
+        public static int Minus(int i, int j)
+        {
+            return i-j;
+        }
+        public static int Subb(int i, int j)
+        {
+            return i*j;
+        }
+        public static int Per(int i, int j)
+        {
+            return i/j;
+        }
+
+    }
+}
---
+++ Project/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Automatikusan kiir mindent, ");
+            System.Console.WriteLine("plussz, minusz, szorzas, osztas");
+
+            int x = 20;
+            int y = 5;
+
+            int osszeadas = Class1.Add(x, y);
+            int kivonas = Class1.Minus(x, y);
+            int szorzass = Class1.Subb(x, y);
+            int osztass = Class1.Per(x, y);
+
+            System.Console.WriteLine("Az ertekek: {0}, {1}, {2}, {3}", osszeadas, kivonas, szorzass, osztass);
+        }
+    }
+}
